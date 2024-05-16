a344c8743a70130268d12972c0c336139bc9e317
Sebah Abubeker <sebahismail21@gmail.com>
2024-05-16 16:16:14 +0200

    - Defined DB and SecretKey .env - Modified function call in server.ts - Changed the const mongoURI in db. - Status: Mongodb Connected and npm run dev without issue.

--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -1,11 +1,14 @@
 import { connect } from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const connectSportDB = async () => {
   try {
-    const mongoURI: string = "mongodb+srv://activitybuddiess:hO2sOrrjaBIRtLDq@sports.mpzqmrl.mongodb.net/SportDB";
+    const mongoURI: string = process.env.MONGODB_URL || "";
     await connect(mongoURI);
     console.log("MongoDB Connected...");
-  } catch (err:any) {
+  } catch (err: any) {
     console.error(err.message);
     // Exit process with failure
     process.exit(1);
@@ -15,4 +18,4 @@ const connectSportDB = async () => {
 // Om vi vill connecta till mer än en databas behöver vi egna funktioner för dem.
 // export { connectSportDB, connectUserDB };
 
-export default connectSportDB();
+export default connectSportDB;
