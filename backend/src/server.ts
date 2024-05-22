f6eb17a385e7c1a8bcab781777b324c70e251a73
Matilda Källström <matilda.kallstrom@hotmail.se>
2024-05-22 15:16:07 +0200

    removed () from db connection

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -5,7 +5,7 @@ import connectSportDB from "../src/db/db";
 dotenv.config();
 const port = process.env.PORT || 3000;
 
-connectSportDB();
+connectSportDB;
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
