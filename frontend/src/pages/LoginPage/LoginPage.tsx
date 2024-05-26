fc81fa7809a8f9a91b504906b1701f102b1f74fc
Denize <denize.yildirim@chasacademy.se>
2024-05-27 00:55:01 +0200

    added usestate to see if box is checked for 'remember me' loginpage, small change of design, routed from login to register

--- frontend/src/pages/LoginPage/LoginPage.tsx
+++ frontend/src/pages/LoginPage/LoginPage.tsx
@@ -1,64 +1,15 @@
-// import React, { useState } from "react";
-
-// const LoginPage: React.FC = () => {
-//   const [username, setUsername] = useState("");
-//   const [password, setPassword] = useState("");
-
-//   const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-//     setUsername(e.target.value);
-//   };
-
-//   const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-//     setPassword(e.target.value);
-//   };
-
-//   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-//     e.preventDefault();
-//     // Handle form submission logic here
-//     console.log("Username:", username);
-//     console.log("Password:", password);
-//   };
-
-//   return (
-//     <>
-//       <h1 className="text-lg font-bold mb-4">LOGGA IN HÄR</h1>
-//       <div className="absolute w-[630px] h-[450px] left-1/2 transform -translate-x-1/2 top-[237.39px]" />
-//       <form onSubmit={handleSubmit}>
-//         <div className="space-y-4 mt-4">
-//           <label htmlFor="username">Användarnamn:</label>
-//           <input
-//             type="text"
-//             id="username"
-//             value={username}
-//             onChange={handleUsernameChange}
-//           />
-//         </div>
-//         <br></br>
-//         <div className="space-y-4 mt-4">
-//           <label htmlFor="password">Lösenord:</label>
-//           <br></br>
-//           <input
-//             type="password"
-//             id="password"
-//             value={password}
-//             onChange={handlePasswordChange}
-//           />
-//         </div>
-//         <br></br>
-//         <button type="submit">Login</button>
-//       </form>
-//     </>
-//   );
-// };
-
-// export default LoginPage;
-
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const LoginPage: React.FC = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const [checked, setChecked] = React.useState(false);
+
+  const handleChange = () => {
+    setChecked(!checked);
+    console.log("Checkbox state:", !checked);
+  };
 
   const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setUsername(e.target.value);
@@ -77,16 +28,13 @@ const LoginPage: React.FC = () => {
 
   return (
     <>
-      <h1 className="text-center mb-4 display font-bold text-2xl leading-12 textColor">
+      <h1 className="text-center mb-4 font-bold text-2xl leading-12">
         Logga in
       </h1>
       <div className="absolute w-[630px] h-[450px] left-1/2 transform -translate-x-1/2 top-[237.39px] bg-gray shadow-md rounded-md p-8">
         <form onSubmit={handleSubmit} className="space-y-4">
           <div className="flex flex-col items-center space-y-3">
-            <label
-              htmlFor="username"
-              className="text-sm font-medium textColor display"
-            >
+            <label htmlFor="username" className="text-sm font-medium">
               Användarnamn:
             </label>
             <input
@@ -98,7 +46,7 @@ const LoginPage: React.FC = () => {
             />
           </div>
           <div className="flex flex-col items-center space-y-2">
-            <label htmlFor="password" className="text-sm font-medium textColor">
+            <label htmlFor="password" className="text-sm font-medium">
               Lösenord:
             </label>
             <input
@@ -109,9 +57,13 @@ const LoginPage: React.FC = () => {
               className="w-2/5 p-2 border border-buttonPurple rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
             />
           </div>
-          <div className="flex justify-around display">
-            <label>
-              <input type="checkbox" />
+          <div className="flex justify-around">
+            <label className="">
+              <input
+                type="checkbox"
+                checked={checked}
+                onChange={handleChange}
+              />
               Kom ihåg mig
             </label>
             <Link
@@ -125,12 +77,12 @@ const LoginPage: React.FC = () => {
             type="submit"
             className="w-2/5 py-2 bg-buttonPurple text-white font-semibold rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
           >
-            Login
+            Logga in
           </button>
           <div className="">
             Har du inget konto?{" "}
             <Link
-              to=""
+              to="/register"
               className="underline hover:underline-offset-4 font-bold"
             >
               Registrera dig här!
