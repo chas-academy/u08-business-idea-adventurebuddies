2a9403443a4e8ba05521d7b2df7afd8be1780130
Denize <denize.yildirim@chasacademy.se>
2024-05-28 00:32:11 +0200

    added const formdata, handle inputChange, changed old inputs to new design

--- frontend/src/pages/RegisterPage/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage/RegisterPage.tsx
@@ -1,11 +1,28 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import Input from "../../components/input/Input";
 
 const RegisterPage: React.FC = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+    email: "",
+    confirmPassword: "",
+    dateOfBirth: "",
+    // phoneNumer: "", SKA VI SKAPA DET I DB?
+  });
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    // Hantera formulär här
   };
 
   return (
@@ -15,62 +32,62 @@ const RegisterPage: React.FC = () => {
       </h1>
       <div className="box-border md:box-content bg-gray shadow-md rounded-md p-8">
         <form onSubmit={handleSubmit} className="space-y-4">
-          <div className="flex flex-col space-y-3">
-            <label htmlFor="username" className="text-sm font-medium">
-              Användarnamn:
-            </label>
-          </div>
-          <input
+          <Input
             type="text"
-            id="username"
-            // value={username}
-            // onChange={handleUsernameChange}
-            className="w-3/5 p-2 border border-buttonPurple rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            label="Användarnamn"
+            name="username"
+            value={formData.username}
+            onChange={handleInputChange}
+            placeholder="Ange användarnamn"
           />
-          <div className="flex flex-col items-center space-y-2">
-            <label htmlFor="password" className="text-sm font-medium">
-              Lösenord:
-            </label>
-          </div>
-          <input
-            type="password"
-            id="password"
-            // value={password}
-            // onChange={handlePasswordChange}
-            className="w-3/5 p-2 border border-buttonPurple rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
-          <div className="flex flex-col space-y-3">
-            <label htmlFor="username" className="text-sm font-medium">
-              Email:
-            </label>
-          </div>
-          <input
+          <Input
             type="text"
-            id="email"
-            // value={username}
-            // onChange={handleUsernameChange}
-            className="w-3/5 p-2 border border-buttonPurple rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            label="Lösenord"
+            name="password"
+            value={formData.password}
+            onChange={handleInputChange}
+            placeholder="Ange lösenord"
           />
-          <div className="flex flex-col space-y-3">
-            <label htmlFor="username" className="text-sm font-medium">
-              Upprepa lösenord:
-            </label>
-          </div>
-          <input
+          <Input
             type="text"
-            id="confirmPassword?"
-            // value={username}
-            // onChange={handleUsernameChange}
-            className="w-3/5 p-2 border border-buttonPurple rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+            label="Email"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+            placeholder="Ange email"
           />
-          <div className="flex justify-around"></div>
+          <Input
+            type="text"
+            label="Upprepa lösenord"
+            name="confirmPassword"
+            value={formData.confirmPassword}
+            onChange={handleInputChange}
+            placeholder="Upprepa lösenord"
+          />
+
+          <Input
+            type="date"
+            label="Ange födelsedatum"
+            name="dateOfBirth"
+            value={formData.confirmPassword}
+            onChange={handleInputChange}
+            placeholder="Ange födelsedatum"
+          />
+          <Input
+            type="text"
+            label="Telefonnummer"
+            name="Date"
+            // value={formData.}
+            onChange={handleInputChange}
+            placeholder="Ange telefonnummer"
+          />
           <button
             type="submit"
-            className="w-2/5 py-2 text-textColor bg-buttonPurple text-white font-semibold rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            className="w-2/5 py-2 text-textColor bg-primaryColor font-semibold rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500"
           >
             Registrera dig här!
           </button>
-          <div className="">
+          <div>
             Har du redan ett konto?{" "}
             <Link
               to="/login"
@@ -98,4 +115,5 @@ const RegisterPage: React.FC = () => {
     </>
   );
 };
+
 export default RegisterPage;
