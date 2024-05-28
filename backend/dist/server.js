31ef7f3959143568f7b2ddabe09dd29550e94384
Sebah Abubeker <sebahismail21@gmail.com>
2024-05-28 23:36:26 +0200

    route fix

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -1,14 +1,16 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
 const dotenv_1 = __importDefault(require("dotenv"));
-const db_1 = __importDefault(require("../src/db/db"));
+const db_1 = __importDefault(require("./db/db"));
 dotenv_1.default.config();
 const port = process.env.PORT || 3000;
 db_1.default;
 app_1.default.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`);
+  console.log(`[server]: Server is running at http://localhost:${port}`);
 });
