050a3753b270b9cee3b9e6c7bfee35f54cb32878
Denize <denize.yildirim@chasacademy.se>
2024-05-28 11:37:02 +0200

    base of register form done, not completed though

--- frontend/src/pages/RegisterPage/RegisterPage.tsx
+++ frontend/src/pages/RegisterPage/RegisterPage.tsx
@@ -9,7 +9,7 @@ const RegisterPage: React.FC = () => {
     email: "",
     confirmPassword: "",
     dateOfBirth: "",
-    // phoneNumer: "", SKA VI SKAPA DET I DB?
+    // phoneNumber: "", SKA VI SKAPA DET I DB?
   });
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -30,8 +30,11 @@ const RegisterPage: React.FC = () => {
       <h1 className="text-center mb-4 font-bold text-2xl leading-12">
         Registrera dig här
       </h1>
-      <div className="box-border md:box-content bg-gray shadow-md rounded-md p-8">
-        <form onSubmit={handleSubmit} className="space-y-4">
+      <div className="flex flex-col justify-center items-center w-full bg-gray shadow-md rounded-md p-8">
+        <form
+          onSubmit={handleSubmit}
+          className="w-4/5 position-self-center space-y-4"
+        >
           <Input
             type="text"
             label="Användarnamn"
@@ -77,7 +80,7 @@ const RegisterPage: React.FC = () => {
             type="text"
             label="Telefonnummer"
             name="Date"
-            // value={formData.}
+            // value={formData.phoneNumber}
             onChange={handleInputChange}
             placeholder="Ange telefonnummer"
           />
@@ -87,30 +90,30 @@ const RegisterPage: React.FC = () => {
           >
             Registrera dig här!
           </button>
-          <div>
-            Har du redan ett konto?{" "}
-            <Link
-              to="/login"
-              className="underline hover:underline-offset-4 font-bold"
-            >
-              Logga in här!
-            </Link>
-          </div>
-          <div>
-            <p>
-              By clicking Register, you agree on our{" "}
-              <a
-                href="https://www.termsandconditions.game/"
-                target="_blank"
-                className="underline hover:underline-offset-4 font-bold"
-                rel="noopener noreferrer"
-              >
-                Privacy Policy
-              </a>{" "}
-              for Adventure Buddies.
-            </p>
-          </div>
         </form>
+        <div>
+          Har du redan ett konto?{" "}
+          <Link
+            to="/login"
+            className="underline hover:underline-offset-4 font-bold"
+          >
+            Logga in här!
+          </Link>
+        </div>
+        <div>
+          <p>
+            By clicking Register, you agree on our{" "}
+            <a
+              href="https://www.termsandconditions.game/"
+              target="_blank"
+              className="underline hover:underline-offset-4 font-bold"
+              rel="noopener noreferrer"
+            >
+              Privacy Policy
+            </a>{" "}
+            for Adventure Buddies.
+          </p>
+        </div>
       </div>
     </>
   );
