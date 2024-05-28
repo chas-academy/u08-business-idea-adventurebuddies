8de6d3e2e0429ee8093177f18f9ba67c13f9796e
Superfelicia <felicia.ahlstrom@hotmail.se>
2024-05-28 12:12:13 +0200

    Date&Time inputs and props and some more.

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {},
     fontFamily: {
-      'poppins': ["Poppins", "sans-serif"],
+      'sans': ["Poppins", "sans-serif"],
     },
     colors: {
       "darkPurple": "#06020D",
