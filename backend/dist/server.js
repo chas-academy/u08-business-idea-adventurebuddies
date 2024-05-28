c2d4a1cc1355dfbbea9ac86fa2bc7c64eb8cead5
Sebah Abubeker <sebahismail21@gmail.com>
2024-05-28 23:57:21 +0200

    base url and db route fix.

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -1,9 +1,7 @@
 "use strict";
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
 const dotenv_1 = __importDefault(require("dotenv"));
@@ -12,5 +10,5 @@ dotenv_1.default.config();
 const port = process.env.PORT || 3000;
 db_1.default;
 app_1.default.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+    console.log(`[server]: Server is running at http://localhost:${port}`);
 });
