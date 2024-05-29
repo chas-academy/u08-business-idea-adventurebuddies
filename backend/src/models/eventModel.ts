bdba214f926af962a1fe730d2a27591dc0fe5727
Matilda Källström <matilda.kallstrom@hotmail.se>
2024-05-29 20:31:28 +0200

    la till mongoose populate

--- backend/src/models/eventModel.ts
+++ backend/src/models/eventModel.ts
@@ -1,20 +1,21 @@
-import { Schema, model } from "mongoose";
+import mongoose, { Schema, model } from "mongoose";
 import { IEvent } from "../interfaces/IEvent";
 
 const locationSchema = new Schema({
-    type: {
-      type: String,
-      enum: ['Point'],
-      required: true
-    },
-    coordinates: {
-      type: [String],
-      required: true
-    }
-  });
+  type: {
+    type: String,
+    enum: ["Point"],
+    required: true,
+  },
+  coordinates: {
+    type: [String],
+    required: true,
+  },
+});
 
 const eventSchema = new Schema<IEvent>({
   activity: { type: String, required: true },
+  user_id: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   start_time: { type: Date, default: () => new Date(), required: true },
   location: locationSchema,
   equipment: { type: String, required: true },
@@ -32,6 +33,44 @@ const eventSchema = new Schema<IEvent>({
     type: Date,
     default: Date.now,
   },
+  venue: {
+    type: String,
+    enum: ["Inomhus", "Utomhus", "Online"],
+    required: true,
+  },
+  gender: {
+    type: String,
+    enum: ["female", "male", "other"],
+    required: true,
+  },
+  language: {
+    type: String,
+    enum: ["svenska", "engelska"],
+    required: true,
+  }, 
+   price: {
+    type: Number,
+    required: true,
+  },
+  experience: {
+    type: String,
+    enum: ["nybörjare", "mellanliggande", "avancerad"],
+    required: true,
+  },
+});
+
+eventSchema.pre("save", async function (next) {
+  try {
+    // Use Mongoose model to find and populate user information
+    const user = await mongoose.model("User").findById(this.user_id);
+    if (user) {
+      this.userName = user.name;
+      this.userEmail = user.email;
+    }
+    next();
+  } catch (error: any) {
+    next(error);
+  }
 });
 
 const Event = model<IEvent>("Event", eventSchema);
