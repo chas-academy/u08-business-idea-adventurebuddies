68b758c332eb4d3c2207a1c7702733c2f4257a71
Sebah Abubeker <sebahismail21@gmail.com>
2024-05-29 15:59:03 +0200

    npm run build

--- backend/dist/models/eventModel.js
+++ backend/dist/models/eventModel.js
@@ -5,26 +5,28 @@ const mongoose_1 = require("mongoose");
 const locationSchema = new mongoose_1.Schema({
     type: {
         type: String,
-        enum: ['Point'],
-        required: true
+        enum: ["Point"],
+        required: true,
     },
     coordinates: {
         type: [Number],
-        required: true
-    }
+        required: true,
+    },
 });
 const eventSchema = new mongoose_1.Schema({
     activity: { type: String, required: true },
-    user_id: { type: mongoose_1.Schema.Types.ObjectId, ref: 'User', required: true },
+    user_id: { type: mongoose_1.Schema.Types.ObjectId, ref: "User", required: true },
     start_time: { type: Date, default: () => new Date(), required: true },
     end_time: { type: Date, default: () => new Date(), required: true },
     location: locationSchema,
     equipment: { type: String, required: true },
-    age: { type: Number, required: false },
+    age: {
+        type: String,
+        enum: ["18-25", "25-35", "35-45", "45-55", "55-65", "65+", "18+"],
+        required: false,
+    },
     totalParticipant: { type: Number, required: true },
-    participants: [
-        { type: mongoose_1.Schema.Types.ObjectId, ref: 'User', required: false },
-    ],
+    participants: [{ type: mongoose_1.Schema.Types.ObjectId, ref: "User", required: false }],
     message: { type: String, required: false },
     created_at: {
         type: Date,
@@ -33,6 +35,30 @@ const eventSchema = new mongoose_1.Schema({
     updated_at: {
         type: Date,
         default: Date.now,
+    },
+    venue: {
+        type: String,
+        enum: ["Inomhus", "Utomhus", "Online"],
+        required: true,
+    },
+    gender: {
+        type: String,
+        enum: ["female", "male", "other"],
+        required: true,
+    },
+    language: {
+        type: String,
+        enum: ["svenska", "engelska"],
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+    experience: {
+        type: String,
+        enum: ["nybörjare", "mellanliggande", "avancerad"],
+        required: true,
     },
 });
 const Event = (0, mongoose_1.model)("Event", eventSchema);
