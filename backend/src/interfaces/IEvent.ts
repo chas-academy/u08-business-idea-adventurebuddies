2a50a927aecae1478a2204f5ff424e795666c8cf
Sebah Abubeker <sebahismail21@gmail.com>
2024-05-29 12:56:22 +0200

    added extra prefrence fields

--- backend/src/interfaces/IEvent.ts
+++ backend/src/interfaces/IEvent.ts
@@ -2,22 +2,38 @@ import { Date } from "mongoose";
 import { ObjectId } from "mongodb";
 
 interface Location {
-    type: 'Point';
-    coordinates: [number, number];
-  }
+  type: "Point";
+  coordinates: [number, number];
+}
+
+type AgeRange =
+  | "18-25"
+  | "25-35"
+  | "35-45"
+  | "45-55"
+  | "55-65"
+  | "65+"
+  | "18+"
+  | string;
 
 export interface IEvent {
-    _id: ObjectId;
-    activity: string;
-    user_id: ObjectId;
-    start_time: Date;
-    end_time: Date;
-    location: Location;
-    equipment: string;
-    age: number;
-    totalParticipant: number;
-    participants: ObjectId[];
-    message: string;
-    created_at: Date;
-    updated_at: Date;
+  _id: ObjectId;
+  activity: string;
+  user_id: ObjectId;
+  start_time: Date;
+  end_time: Date;
+  location: Location;
+  equipment: string;
+  age: AgeRange;
+  totalParticipant: number;
+  participants: ObjectId[];
+  message: string;
+  created_at: Date;
+  updated_at: Date;
+
+  venue: "Inomhus" | "Utomhus" | "Online";
+  gender: "female" | "male" | "other";
+  language: "svenska" | "engelska";
+  price: number;
+  experience: "nybörjare" | "mellanliggande" | "avancerad";
 }
