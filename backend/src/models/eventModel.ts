e735c27fbdcde5d12fd7022ba24f913cf3a15692
Sebah Abubeker <sebahismail21@gmail.com>
2024-05-29 18:30:30 +0200

    Interface modifications

--- backend/src/models/eventModel.ts
+++ backend/src/models/eventModel.ts
@@ -15,54 +15,53 @@ const locationSchema = new Schema({
   },
 });
 
-const eventSchema = new Schema<IEvent>({
-  activity: { type: String, required: true },
-  user_id: { type: Schema.Types.ObjectId, ref: "User", required: true },
-  start_time: { type: Date, default: () => new Date(), required: true },
-  end_time: { type: Date, default: () => new Date(), required: true },
-  location: locationSchema,
-  equipment: { type: String, required: true },
-  age: {
-    type: String,
-    enum: ["18-25", "25-35", "35-45", "45-55", "55-65", "65+", "18+"],
-    required: false,
+const eventSchema = new Schema<IEvent>(
+  {
+    activity: { type: String, required: true },
+    user_id: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    start_time: { type: Date, default: () => new Date(), required: true },
+    end_time: { type: Date, default: () => new Date(), required: true },
+    location: { type: String, required: true },
+    equipment: { type: String, required: true },
+    age: { type: Number, required: false },
+    totalParticipant: { type: Number, required: true },
+    participantsMin: { type: Number, required: true },
+    participantsMax: { type: Number, required: true },
+    participants: [
+      { type: Schema.Types.ObjectId, ref: "User", required: false },
+    ],
+    lat: { type: String, required: true },
+    lon: { type: String, required: true },
+    message: { type: String, required: false },
+    venue: {
+      type: String,
+      enum: ["Inomhus", "Utomhus", "Online"],
+      required: true,
+    },
+    gender: {
+      type: String,
+      enum: ["female", "male", "other"],
+      required: true,
+    },
+    language: {
+      type: String,
+      enum: ["svenska", "engelska"],
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    experience: {
+      type: String,
+      enum: ["nybörjare", "mellanliggande", "avancerad"],
+      required: true,
+    },
   },
-  totalParticipant: { type: Number, required: true },
-  participants: [{ type: Schema.Types.ObjectId, ref: "User", required: false }],
-  message: { type: String, required: false },
-  created_at: {
-    type: Date,
-    default: Date.now,
-  },
-  updated_at: {
-    type: Date,
-    default: Date.now,
-  },
-  venue: {
-    type: String,
-    enum: ["Inomhus", "Utomhus", "Online"],
-    required: true,
-  },
-  gender: {
-    type: String,
-    enum: ["female", "male", "other"],
-    required: true,
-  },
-  language: {
-    type: String,
-    enum: ["svenska", "engelska"],
-    required: true,
-  },
-  price: {
-    type: Number,
-    required: true,
-  },
-  experience: {
-    type: String,
-    enum: ["nybörjare", "mellanliggande", "avancerad"],
-    required: true,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Event = model<IEvent>("Event", eventSchema);
 
