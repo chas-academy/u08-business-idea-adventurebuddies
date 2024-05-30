f2bf699912b96c6d7b41e8a30172125359210109
Sebah Abubeker <sebahismail21@gmail.com>
2024-05-30 18:22:13 +0200

    read and get event by query, price is string and has enum,

--- backend/src/routes/events.ts
+++ backend/src/routes/events.ts
@@ -9,18 +9,20 @@ import {
   removeEventFromUserList,
   attend,
   unattend,
+  getEventsByQuery,
 } from "../controllers/eventcontroller";
 
 const eventRouter = Router();
 
-eventRouter.get("/", getAllEvents);
-eventRouter.post("/", createEvent);
-eventRouter.get("/:id", getEventById);
-eventRouter.put("/:id", updateEvent);
-eventRouter.delete("/:id", deleteEvent);
-eventRouter.post("/:userId/add/:eventId", addEventToUserList);
-eventRouter.delete("/:userId/remove/:eventId", removeEventFromUserList);
-eventRouter.post("/:userId/attend/:eventId", attend);
-eventRouter.post("/:userId/unattend/:eventId", unattend);
+eventRouter.get("/", getAllEvents); // http://localhost:3000/api/events
+eventRouter.get("/query", getEventsByQuery); // query string example:http://localhost:3000/api/events/query?gender=Femal&venue=Online
+eventRouter.post("/", createEvent); // http://localhost:3000/api/events
+eventRouter.get("/:id", getEventById); // http://localhost:3000/api/events/eventId
+eventRouter.put("/:id", updateEvent); // http://localhost:3000/api/events/eventId
+eventRouter.delete("/:id", deleteEvent); // http://localhost:3000/api/events/eventId
+eventRouter.post("/:userId/add/:eventId", addEventToUserList); // http://localhost:3000/api/events/userId/add/eventId
+eventRouter.delete("/:userId/remove/:eventId", removeEventFromUserList); // http://localhost:3000/api/events/userId/remove/eventId
+eventRouter.post("/:userId/attend/:eventId", attend); // http://localhost:3000/api/events/userId/attend/eventId
+eventRouter.post("/:userId/unattend/:eventId", unattend); // http://localhost:3000/api/events/userId/unattend/eventId
 
 export default eventRouter;
