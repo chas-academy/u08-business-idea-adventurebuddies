b059102c44342858f5cba7c23b06b16298166dc0
HarveyBong <felix.nagy@chasacademy.se>
2024-05-30 09:22:23 +0200

    Last Tabs fixes

--- frontend/src/pages/EventInfoPage/EventInfoPage.tsx
+++ frontend/src/pages/EventInfoPage/EventInfoPage.tsx
@@ -1,9 +1,21 @@
+import Tab from "../../components/tabs/Tab";
+import Tabs from "../../components/tabs/Tabs";
+import { TabsProvider } from "../../components/tabs/TabsContext";
+
 const EventInfoPage = () => {
 
     return (
-        <>
+        <TabsProvider >
+        <Tabs>
+       
+        <Tab tabLabel="Mina Events"><div>hej event1</div></Tab>
+        <Tab tabLabel="Alla Events"><div>hej event 2</div></Tab>
+        
+        <Tab tabLabel="Test Test Test"><div>hej event 3</div></Tab>
         Event info
-        </>
+        </Tabs>
+        
+        </TabsProvider>
     )
 }
 
