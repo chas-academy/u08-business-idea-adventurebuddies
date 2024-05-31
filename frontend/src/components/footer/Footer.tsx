94bafe7b6e50d93125ef816314e09b9da7a7425b
Matilda Källström <matilda.kallstrom@hotmail.se>
2024-05-31 10:22:58 +0200

    smalelr changes

--- frontend/src/components/footer/Footer.tsx
+++ frontend/src/components/footer/Footer.tsx
@@ -4,11 +4,6 @@ import { faSquareFacebook } from "@fortawesome/free-brands-svg-icons/faSquareFac
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons/faLinkedin";
 import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons/faArrowUpRightFromSquare";
 import { faCopyright } from "@fortawesome/free-solid-svg-icons/faCopyright";
-import { faHouseChimneyWindow } from "@fortawesome/free-solid-svg-icons/faHouseChimneyWindow";
-import { faMap } from "@fortawesome/free-solid-svg-icons/faMap";
-import { faPlus } from "@fortawesome/free-solid-svg-icons/faPlus";
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons/faMagnifyingGlass";
-import { faUserLarge } from "@fortawesome/free-solid-svg-icons/faUserLarge";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons/faInstagram";
 import React from "react";
 
@@ -27,8 +22,8 @@ const Footer = () => {
               className="w-full h-auto"
             >
               <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
+                fillRule="evenodd"
+                clipRule="evenodd"
                 d="M0 144.19L260 0L483.079 200.765L576 164.718L638.885 200.765L853.318 61.5807L1152 283.876L1440 237.314V376.999H1152H864H576H288H0V144.19Z"
                 fill="#B5C0F8"
               />
@@ -44,8 +39,8 @@ const Footer = () => {
               className="w-full h-auto"
             >
               <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
+                fillRule="evenodd"
+                clipRule="evenodd"
                 d="M0 42.998L30 56.998C60 71.998 120 99.998 180 107.998C240 114.998 300 99.998 360 78.998C420 56.998 480 27.998 540 49.998C600 71.998 660 143.998 720 135.998C780 128.998 840 42.998 900 35.998C960 27.998 1020 99.998 1080 92.998C1140 85.998 1200 -0.00195312 1260 -0.00195312C1320 -0.00195312 1380 85.998 1410 128.998L1440 171.998V301.998H1410C1380 301.998 1320 301.998 1260 301.998C1200 301.998 1140 301.998 1080 301.998C1020 301.998 960 301.998 900 301.998C840 301.998 780 301.998 720 301.998C660 301.998 600 301.998 540 301.998C480 301.998 420 301.998 360 301.998C300 301.998 240 301.998 180 301.998C120 301.998 60 301.998 30 301.998H0V42.998Z"
                 fill="#828FD9"
               />
@@ -61,8 +56,8 @@ const Footer = () => {
               className="w-full h-auto"
             >
               <path
-                fill-rule="evenodd"
-                clip-rule="evenodd"
+                fillRule="evenodd"
+                clipRule="evenodd"
                 d="M0 43L34 58C69 72 137 101 206 144C274 187 343 245 411 295C480 346 549 389 617 374C686 360 754 288 823 252C891 216 960 216 1029 230C1097 245 1166 274 1234 238C1303 202 1371 101 1406 50L1440 0V432H1406C1371 432 1303 432 1234 432C1166 432 1097 432 1029 432C960 432 891 432 823 432C754 432 686 432 617 432C549 432 480 432 411 432C343 432 274 432 206 432C137 432 69 432 34 432H0V43Z"
                 fill="url(#paint0_linear_518_6743)"
               />
@@ -75,8 +70,8 @@ const Footer = () => {
                   y2="458"
                   gradientUnits="userSpaceOnUse"
                 >
-                  <stop stop-color="#5463AF" />
-                  <stop offset="1" stop-color="#21274C" />
+                  <stop stopColor="#5463AF" />
+                  <stop offset="1" stopColor="#21274C" />
                 </linearGradient>
               </defs>
             </svg>
