f4e9bc251621b3825d3c74347a7175e63855d5ad
Mikael Jakobsson <Maadmike@hotmail.se>
2024-06-03 17:38:54 +0200

    FormEvent säkrad, Mpas går att byta och håller värdet

--- frontend/src/pages/homePage/HomePage.tsx
+++ frontend/src/pages/homePage/HomePage.tsx
@@ -3,6 +3,7 @@ import MapPage from "../map/MapPage";
 import React, { useEffect, useState } from "react";
 import SearchBar from "../map/SearchBar";
 import { IEvent } from "../../../../backend/src/interfaces/IEvent";
+import Maps2 from "../map/Maps2";
 
 const HomePage = () => {
   const [events, setEvents] = useState<IEvent[]>([]);
@@ -11,22 +12,22 @@ const HomePage = () => {
   useEffect(() => {
     const fetchEvents = async () => {
       try {
-      // const urlParams = new URLSearchParams(window.location.search);
-      // const url = `http://localhost:3000/api/events/query?${urlParams.toString()}`;
-      const response = await fetch('http://localhost:3000/api/events');
-      const data = await response.json();
-      setEvents(data);
-      setFilteredEvents(data);
+        // const urlParams = new URLSearchParams(window.location.search);
+        // const url = `http://localhost:3000/api/events/query?${urlParams.toString()}`;
+        const response = await fetch("http://localhost:3000/api/events");
+        const data = await response.json();
+        setEvents(data);
+        setFilteredEvents(data);
 
-      // if (data) {
-      //   setEvents(data);
-      // } else {
-      //   console.error('Backend returned empty response');
-      // }
+        // if (data) {
+        //   setEvents(data);
+        // } else {
+        //   console.error('Backend returned empty response');
+        // }
       } catch (error) {
-        console.error('Error fetching events:', error);
+        console.error("Error fetching events:", error);
       }
-    }; 
+    };
     fetchEvents();
   }, []);
 
@@ -35,27 +36,31 @@ const HomePage = () => {
       try {
         const decodedUrl = decodeURIComponent(filteredEventsUrl);
         const response = await fetch(decodedUrl);
-        console.log('filtered events response:', response)
+        console.log("filtered events response:", response);
         if (response.ok) {
           const data = await response.json();
           setFilteredEvents(data);
         } else {
           const responseText = await response.text();
-          console.error('Error fetching filtered events:', response.status, response.statusText);
-          console.error('Response data:', responseText);
+          console.error(
+            "Error fetching filtered events:",
+            response.status,
+            response.statusText
+          );
+          console.error("Response data:", responseText);
         }
       } catch (error) {
-        console.error('Error fetching filtered events:', error);
+        console.error("Error fetching filtered events:", error);
       }
     };
 
     fetchFilteredEvents();
-  }
+  };
 
   return (
     <div className="h-screen">
-    <SearchBar />
-    <FilterDropdown events={events} onFilter={handleFilter}  />
+      <Maps2 />
+      <FilterDropdown events={events} onFilter={handleFilter} />
       {/* <>
         <MapPage />
       </> */}
