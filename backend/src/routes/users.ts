3a75d8ed554f20706ea2ec80889add9d08ae8cef
Matilda Källström <matilda.kallstrom@hotmail.se>
2024-06-03 18:39:12 +0200

    needed to change update (put) user for it to work when you added a picture

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -98,10 +98,24 @@ userRouter.get("/", auth, admin, async (req, res) => {
 });
 
 userRouter.put("/:id", auth, async (req, res) => {
-  const id = req.params.id;
-  const updatedUser = await updateUser(id, req.body);
-  res.status(200).json({ message: "Update succeeded", updatedUser });
-});
+    try {
+      const id = req.params.id;
+      let userData = req.body;
+
+      // Kollar om det finns en uppladad fil (vi använder multer för filuppladningar)
+      if (req.file) {
+        userData.profileImageUrl = req.file.filename; // Tilldela sökvägen till profileImageUrl
+      }
+
+      const updatedUser = await updateUser(id, userData);
+
+      res.status(200).json({ message: "Update succeeded", updatedUser });
+    } catch (error) {
+      console.error("Error updating user:", error);
+      res.status(500).json({ message: "Internal Server Error" });
+    }
+  }
+);
 
 userRouter.delete("/me", auth, deleteOwnAccount);
 
