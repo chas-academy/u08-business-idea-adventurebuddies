6f44c2071ddfa5dc0b22c7d799cbf2ce01690c15
Sebah Abubeker <sebahismail21@gmail.com>
2024-06-04 12:55:05 +0200

    Commented children and react nodejs for the time being.

--- frontend/src/components/tabs/Tab.tsx
+++ frontend/src/components/tabs/Tab.tsx
@@ -1,34 +1,40 @@
 import { useTabs } from "./TabsContext";
 import "./Tabs.css";
-import React, { Dispatch, ReactNode, SetStateAction } from "react";
+import React, {
+  Dispatch,
+  // ReactNode,
+  SetStateAction,
+} from "react";
 
 interface TabProps {
-  children: ReactNode;
+  // children: ReactNode;
   tabLabel: string;
   index: number;
   setActiveTabIndex: Dispatch<SetStateAction<number>>;
 }
 
-const Tab: React.FC<TabProps> = ({ children, tabLabel, index }) => {
-  const { activeTab,setActiveTab } = useTabs();
+const Tab: React.FC<TabProps> = ({
+  // children,
+  tabLabel,
+  index,
+}) => {
+  const { activeTab, setActiveTab } = useTabs();
   const handleTabClick = () => {
-    setActiveTab(index); 
-    console.log("ACTIVEE TTAAABABBB ", index)
+    setActiveTab(index);
+    console.log("ACTIVEE TTAAABABBB ", index);
   };
   return (
-    
     <div
-      className={
-        `tabs flex-1 drop bg-textColor ${activeTab === index
+      className={`tabs flex-1 drop bg-textColor ${
+        activeTab === index
           ? " rounded-t-lg drop-shadow-md tabs active "
-          : "rounded-t-lg  " }`
-      }
+          : "rounded-t-lg  "
+      }`}
       onClick={handleTabClick}
     >
       {tabLabel}
-      {children}
+      {/* {children} */}
     </div>
-    
   );
 };
 export default Tab;
